d2409f9ae10eae6ae73db4ceff20cdce105f624b
ccl71102 <colonlopezcarlos@gmail.com>
2019-07-22 14:45:01 -0600

    initial version

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 7100
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/pizzadb",
+{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+.then( () => console.log("Reticulating pies..."))
+.catch( err => console.log(err));
+
+app.use("/pizza", require("./routes/pizzaRouter.js"));
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    return res.send({errorMessage: err.message});
+})
+
+app.listen(PORT, () => {
+    console.log(`Setting oven to ${PORT} degrees`);
+});
