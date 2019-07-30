b336833629ba7e4439c83d35969c5d8db737f1b3
ccl71102 <colonlopezcarlos@gmail.com>
2019-07-30 17:12:32 -0600

    phone

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 7100
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 mongoose.connect("mongodb://localhost:27017/oven",
 {
@@ -22,6 +24,10 @@ app.use((err, req, res, next) => {
     console.log(err);
     return res.send({errorMessage: err.message});
 })
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Setting oven to ${PORT} degrees`);
