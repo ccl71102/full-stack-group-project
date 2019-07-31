a4ede373a26ebc817ad043a123c32f511d51a39e
ccl71102 <colonlopezcarlos@gmail.com>
2019-07-31 17:32:48 -0600

    done

--- client/src/components/OrderInput.js
+++ client/src/components/OrderInput.js
@@ -1,41 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 
-class OrderInput extends Component {
+const OrderInput = props => {
 
-    constructor(){
-        super();
-        this.state = {
-            ammount: 0
-        }
-    }
-
-    handleChange = e => {
-        const {name, value} = e.target;
-        if(!isNaN(value) && !value.includes(".")) {
-            this.setState({
-                [name]: value
-            });
-        }
-    }
-
-    render(){
         return (
                 <> 
-                    <button className="orderAmont" onClick={() => this.props.increaseCount(this.props._id, this.state.ammount)}><FontAwesomeIcon icon={faPlusCircle}/></button>
-                    <input  onChange={this.handleChange} 
-                            type="number" 
-                            name="ammount" 
-                            value={this.state.ammount || ""}
-                            placeholder="Ammount To Change"
-                            min="0"
-                            max="1000000" 
-                    />
-                    <button className="orderAmont" onClick={() => this.props.decreaseCount(this.props._id, this.state.ammount)}><FontAwesomeIcon icon={faMinusCircle}/></button> 
+                    <button className="orderAmont" onClick={() => props.increaseCount(props._id, 1)}><FontAwesomeIcon icon={faPlusCircle}/></button>
+                    <button className="orderAmont" onClick={() => props.decreaseCount(props._id, 1)}><FontAwesomeIcon icon={faMinusCircle}/></button> 
                 </>
         )
-    }
 
 }
 
