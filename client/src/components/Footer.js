27697d1d690d82bae1a3dddef979d7b990ff2caa
ccl71102 <colonlopezcarlos@gmail.com>
2019-08-01 09:52:37 -0600

    bottom text

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return(
         <div className="footer">
             <div className="footerDiv">
-                <h4 className="footerText">Call to Order: (888) 555-PIZA</h4>
-                <h4 className="footerText">Located At: 150 South State Street,<br/> Salt Lake City,<br/> UT 84111</h4>
+                <h4 className="footerText">Call to Order:<br/>(801) 555-PZZA</h4>
+                <h4 className="footerText">Located At:<br/>150 South State Street<br/>Salt Lake City, UT 84111</h4>
             </div>
         </div>
     )
